837895f284b21ad53ee4ef50673898a4e28d3020
oorcun <orcunaltinsoy@gmail.com>
2023-03-10 02:09:13 +0300

    Added init.

---
+++ init.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/oorcun/docker.git new
+cd new
+docker run -d --name php oorcun/laravel:php
+docker cp php:/srv/laravel/.env .env
+docker cp php:/srv/laravel/vendor vendor
+docker rm -f php
+printf "\nDOCKER_USER_ID=$(id -u)\nDOCKER_GROUP_ID=$(id -g)\n" >> .env
